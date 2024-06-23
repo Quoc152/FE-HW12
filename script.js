0980a14e3e371d6c772845cada91f35c5f4cf68b
Quoc Le <quocle152@gmail.com>
2024-06-23 22:26:17 +0700

    edit submit even

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-function handleSubmit() {
-    let ten = document.getElementById("ten").value;
-    let mssv = document.getElementById("mssv").value;
-    let email = document.getElementById("email").value;
-    let khoa = document.getElementById("khoa").value;
-    let gioitinh = document.getElementById("gioitinh").value;
-    let ngaysinh = document.getElementById("ngaysinh").value;
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('studentForm').addEventListener('submit', (event) => {
+        event.preventDefault();
 
-    let sinhvien =   {
-        Ten: ten,
-        MSSV: mssv,
-        Email: email,
-        Khoa: khoa,
-        GioiTinh: gioitinh,
-        NgaySinh: ngaysinh
-    };
+        let ten = document.getElementById('ten').value;
+        let mssv = document.getElementById('mssv').value;
+        let email = document.getElementById('email').value;
+        let khoa = document.getElementById('khoa').value;
+        let gioitinh = document.getElementById('gioitinh').value;
+        let ngaysinh = document.getElementById('ngaysinh').value;
 
-    console.log(sinhvien);
-}
+        let sinhvien = {
+            Ten: ten,
+            MSSV: mssv,
+            Email: email,
+            Khoa: khoa,
+            Gioitinh: gioitinh,
+            Ngaysinh: ngaysinh
+        }
+
+        if (!ten || !mssv || !email || !khoa || !gioitinh || !ngaysinh) {
+            alert("Vui lòng điền đầy đủ thông tin!");
+            return false; 
+        }
+
+        console.log(sinhvien);
+        alert("Biểu mẫu đã được gửi thành công!");
+
+        return true; 
+    });
+});
